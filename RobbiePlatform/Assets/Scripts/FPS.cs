a0501ba2413c6b05c3df7a67d7a50769c8ada473
Chongwen <70866088+BiuWcs@users.noreply.github.com>
2021-05-28 18:44:29 -0700

    Update FPS.cs

--- RobbiePlatform/Assets/Scripts/FPS.cs
+++ RobbiePlatform/Assets/Scripts/FPS.cs
@@ -10,11 +10,10 @@ public class FPS : MonoBehaviour
     public Text fpsText;
 
 	float deltaTime;
-	
 	void Update ()
 	{
 		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
-        SetFPS();
+        	SetFPS();
 	}
 
 	void SetFPS()

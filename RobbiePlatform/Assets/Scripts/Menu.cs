4b66c72c49e55f2962d2b417351a449dda88b58d
Chongwen <70866088+BiuWcs@users.noreply.github.com>
2021-05-28 18:48:13 -0700

    Update Menu.cs

--- RobbiePlatform/Assets/Scripts/Menu.cs
+++ RobbiePlatform/Assets/Scripts/Menu.cs
@@ -6,19 +6,20 @@ using UnityEngine.Audio;
 public class Menu : MonoBehaviour
 {
     public GameObject pauseMenu;
-
+    
+    // game pause
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
-
+    // continue game
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
-
+    // quit game
     public void QuitGame()
     {
         Application.Quit();

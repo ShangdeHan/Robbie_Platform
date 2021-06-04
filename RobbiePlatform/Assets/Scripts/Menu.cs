61bffab9ce89819cc14bc116f7bb70b2db082152
ShangdeHan <shangdehan@gmail.com>
2021-06-03 17:08:29 -0700

    final fix

--- RobbiePlatform/Assets/Scripts/Menu.cs
+++ RobbiePlatform/Assets/Scripts/Menu.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class Menu : MonoBehaviour
-{
+public class Menu : MonoBehaviour {
+
+    public bool Paused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                ResumeGame();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+
     public GameObject pauseMenu;
-    
+
     // game pause
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        Paused = true;
     }
     // continue game
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        Paused = false;
     }
     // quit game
     public void QuitGame()
